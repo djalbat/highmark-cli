755948740bdcedbffdfc45d18f6c3ed032a0aab7
James Smith <james.smith@djalbat.com>
2024-05-09 19:54:09 +0100

    Gotten the overlay child div elements working properly.

--- src/utilities/element.js
+++ src/utilities/element.js
@@ -11,11 +11,31 @@ export function elementsFromDOMElements(domElements, Element) {
 }
 
 export function elementFromDOMElement(domElement, Element) {
-  const element = <Element/>;
+  const element =
+
+          <Element/>
+
+        ,
+        className = classNameFromElement(element);
+
+  addClassNameToDOMElement(domElement, className)
 
   element.domElement = domElement;  ///
 
   domElement.__element__ = element; ///
 
   return element;
+}
+
+function classNameFromElement(element) {
+  const domElement = element.getDOMElement(),
+        { className } = domElement;
+
+  return className;
+}
+
+function addClassNameToDOMElement(domElement, className) {
+  const { classList } = domElement;
+
+  classList.add(className);
 }
