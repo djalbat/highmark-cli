481b1a6517f517a9785a7b201603c24995d20601
James Smith <james.smith@djalbat.com>
2025-11-12 19:39:13 +0000

    Propagated.

--- src/operation/copyClientFiles.js
+++ src/operation/copyClientFiles.js
@@ -1,11 +1,15 @@
 "use strict";
 
-import { copyFiles as copyClientFiles } from "highmark-client";
+import { packageUtilities } from "highmark-client";
+
+const { getClientJSFilePath, getCheckmarkSVGFilepath } = packageUtilities;
 
 export default function copyClientFilesOperation(proceed, abort, context) {
   const { client } = context;
 
   if (client) {
+    const filePath = concatenatePaths(projectDirectoryName, INDEX_HTML_FILE_NAME);
+
     const { projectDirectoryName } = context,
           targetDirectoryPath = projectDirectoryName; ///
 
