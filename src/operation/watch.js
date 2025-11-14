a4025a011caf610f4d9f0e870b9633126a3eb8a3
James Smith <james.smith@djalbat.com>
2025-11-14 17:04:41 +0000

    Added the client JavaScript file to the watch pattern.

--- src/operation/watch.js
+++ src/operation/watch.js
@@ -1,19 +1,26 @@
 "use strict";
 
 import { pathUtilities } from "necessary";
+import { packageUtilities } from "highmark-client";
 import { createLiveReloadHandler } from "lively-cli";
 
 import { LIVE_RELOAD_PATH, INDEX_HTML_FILE_NAME } from "../constants";
 
-const { concatenatePaths } = pathUtilities;
+const { concatenatePaths } = pathUtilities,
+      { getClientJSFileName } = packageUtilities;
 
 export default function watchOperation(proceed, abort, context) {
   const { watch } = context;
 
   if (watch) {
     const { server, quietly, projectDirectoryName } = context,
+          clientJSFileName = getClientJSFileName(),
+          clientJSFilePath = concatenatePaths(projectDirectoryName, clientJSFileName),
           indexHTMLFilePath = concatenatePaths(projectDirectoryName, INDEX_HTML_FILE_NAME),
-          watchPattern = indexHTMLFilePath, ///
+          watchPattern = [
+            clientJSFilePath,
+            indexHTMLFilePath
+          ],
           liveReloadHandler = createLiveReloadHandler(watchPattern, quietly);
 
     server.get(LIVE_RELOAD_PATH, liveReloadHandler);
